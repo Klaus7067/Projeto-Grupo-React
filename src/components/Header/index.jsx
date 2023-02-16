04c6ad83a8652d951ad53baa207beb4f2b71a243
Juan Fernandes <113736913+juanjpf@users.noreply.github.com>
2023-02-15 22:44:38 -0300

    background

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from '../../assets/computador.png'
+import logo from '../../assets/computador.jpg'
 import './header.css'
 
 function Header() {
